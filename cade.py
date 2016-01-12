72f03682bc10d2ed98c87f93d7e5b63c5c75e580
Ryan <rwooster@umich.edu>
2016-01-11 19:58:28 -0500

    Added basic bot framework.
    To run, need to have env vars of DISCORD_CADE_EMAIL and DISCORD_CADE_PASSWORD set for the account the bot should run on.

---
+++ cade.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+import sys
+import discord
+
+import daemon
+
+class Cade(discord.Client):
+
+    # Override the daemon classes run
+
+    def __init__(self):
+        super(Cade, self).__init__()
+
+        # get account info
+        self.email = os.environ['DISCORD_CADE_EMAIL']
+        self.password = os.environ['DISCORD_CADE_PASSWORD']
+
+        # login to discord
+        self.login(self.email, self.password) 
+
+        # start the bot
+        self.run()
+        
+    def on_message(self, message):
+        if message.content.startswith('!hello'):
+            self.send_message(message.channel, 'Hello ' + message.author.name)
+
+    def on_ready(self):
+        print('Logged in as {0}'.format(self.user.name))
+
+if __name__ == "__main__":
+    cade = Cade()
